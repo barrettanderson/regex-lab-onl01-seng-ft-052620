0571285f1ee819e96850329a00b7881477c03b27
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-19 20:24:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -28,7 +28,6 @@ end
 
 def valid_phone_number?(phone)
   if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
-  if phone.match(/\(?([0-9]{3})\)?([ -]?)([0-9]{3})([ -]?)([0-9]{4})/) == nil
     false
   else
     true
