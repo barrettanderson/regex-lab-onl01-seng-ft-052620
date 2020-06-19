f4f09cab66eb25374a5169b521424e6e532dadd4
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-19 19:57:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def starts_with_a_vowel?(word)
-  if word.match(/\A[aeiou])
+  if word.match(/\A[aeiou]/)
     return true
   else
     return false
@@ -9,18 +9,18 @@ def starts_with_a_vowel?(word)
 end
 
 
-# def words_starting_with_un_and_ending_with_ing(text)
-#
-# end
-#
-# def words_five_letters_long(text)
-#
-# end
-#
-# def first_word_capitalized_and_ends_with_punctuation?(text)
-#
-# end
-#
-# def valid_phone_number?(phone)
-#
-# end
+def words_starting_with_un_and_ending_with_ing(text)
+
+end
+
+def words_five_letters_long(text)
+
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+
+end
+
+def valid_phone_number?(phone)
+
+end
